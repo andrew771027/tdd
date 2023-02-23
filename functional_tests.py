d70666e08e47620f406dcf32968a31561f23d97b
Po-Yuan Wang <andrew.wang@onedegree.hk>
2023-02-23 23:19:06 +0800

    chapter 4

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import unittest
 
 
@@ -15,8 +17,23 @@ class NewVisitorTest(unittest.TestCase):
 
         self.browser.get("http://localhost:8000")
 
-        self.assertIn(
-            "To-Do", self.browser.title)
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn("To-Do", header_text)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute(
+            'placeholder'), "Enter a to-do item")
+
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == "1. Buy peacock features' for row in rows" for row in rows),
+            "New to-do item did bot appear in table"
+        )
 
         self.fail('Finish the test!')
 
