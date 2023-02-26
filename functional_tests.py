124a0e61d8c49c1d41983797a744c9bf041d34f0
Po-Yuan Wang <andrew.wang@onedegree.hk>
2023-02-26 23:49:25 +0800

    chapter 5

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_retrieve_it_later(self):
 
         self.browser.get("http://localhost:8000")
@@ -28,12 +33,18 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == "1. Buy peacock features' for row in rows" for row in rows),
-            "New to-do item did bot appear in table"
-        )
+
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table(
+            "2: Use peacock feathers to make a fly")
 
         self.fail('Finish the test!')
 
