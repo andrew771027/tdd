28bd76038deb6e2d171fd3e2d37bc34074be82d7
Po-Yuan Wang <andrew.wang@onedegree.hk>
2023-03-13 22:57:00 +0800

    edit settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$l*od)t-4dh9mrx%ho@+cp@6l6a#sasmj(782!kia0(nho7y%l
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'tdd-superlists.herokuapp.com/'
+    'tdd-superlists.herokuapp.com'
 ]
 
 
