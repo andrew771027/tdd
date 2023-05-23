d6e722afc55179900fe3cf6546bf521058b3ff4f
Po-Yuan Wang <andrew.wang@onedegree.hk>
2023-05-23 22:06:38 +0800

    complete chapter 12

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,7 +1,7 @@
 from .base import FunctionalTest
 from unittest import skip
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
 
@@ -10,7 +10,7 @@ class ItemValidationTest(FunctionalTest):
         # Edith 前往首頁，並不小心試著提交
         # 一個空的清單項目。他在空的輸入方塊中按下Enter
         self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('\n')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         
         # 首頁重新整理，有一個錯誤訊息
         # 說不能有空的清單選項
@@ -18,11 +18,12 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item.")
 
         # 他再試一次，在項目中加入一些文字，現在可以動作了
-        self.get_item_input_box().send_keys('Buy milk\n')
+        self.get_item_input_box().send_keys('Buy milk')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy milk')
 
         # 離譜的是，現在他決定要提交地按個空白的清單項目
-        self.get_item_input_box().send_keys('\n')
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # 他在清單網頁上看到類似的警告
         self.check_for_row_in_list_table('1: Buy milk')
@@ -30,7 +31,8 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item.")
 
         # 她可以填入一些文字來修正它
-        self.get_item_input_box().send_keys('Make tea\n')
+        self.get_item_input_box().send_keys('Make tea')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
     
@@ -38,13 +40,15 @@ class ItemValidationTest(FunctionalTest):
 
         # Edith 前往首頁，開始編輯一個新的清單
         self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('Buy wellies\n')
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy wellies')
 
         # 他不小心試著輸入一個重複的項目
         self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # 他看到一個有用的錯誤訓息
         self.check_for_row_in_list_table('1: Buy wellies')
         error = self.browser.find_element(By.CSS_SELECTOR, '.has-error')
-        self.assertEqual(error.text, "You've already got this in your list")
+        self.assertEqual(error.text, "You've already got this in your list.")
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,4 @@
-from unittest import mock
+from unittest import mock, skip
 
 from django.test import TestCase
 from django.urls import resolve
@@ -8,7 +8,7 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ItemForm, ExistingListItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
 # Create your tests here.
 
 NEW_ITEM = "A new item for an existing list"
@@ -121,8 +121,24 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
+
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data = {'text': 'textey'}
+        )
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
 class NewListTest(TestCase):
 
@@ -155,6 +171,4 @@ class NewListTest(TestCase):
         expected_error = escape(EMPTY_ITEM_ERROR)
         self.assertContains(response, expected_error)
 
-    def test_for_invalid_input_passes_form_to_template(self):
-        response = self.client.post('/lists/new', data={'text': ""})
-        self.assertIsInstance(response.context['form'], ItemForm)
+
